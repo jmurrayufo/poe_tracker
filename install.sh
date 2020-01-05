e4fec707f6d4dd495d67325924308fac8c54fe63
John Murray <jmurrayufo@gmail.com>
2020-01-04 22:37:16 -0700

    Restart on install

--- install.sh
+++ install.sh
@@ -9,24 +9,24 @@ echo "The following commands will be printed to the prompt, and executed after y
 echo "ctrl+c at any time if you don't wish to run the next command!"
 
 echo "About to run: cp poe-tracker.service /etc/systemd/system/poe-tracker.service"
-read
+# read
 cp poe-tracker.service /etc/systemd/system/poe-tracker.service
 
 
 echo "About to run: chmod 644 /etc/systemd/system/poe-tracker.service"
-read
+# read
 chmod 644 /etc/systemd/system/poe-tracker.service
 
 echo "About to run: systemctl daemon-reload"
-read
+# read
 systemctl daemon-reload
 
 echo "About to run: systemctl start poe-tracker"
-read
-systemctl start poe-tracker
+# read
+systemctl restart poe-tracker
 systemctl status poe-tracker
 
 echo "About to run: systemctl enable poe-tracker"
-read
+# read
 systemctl enable poe-tracker
 systemctl status poe-tracker
