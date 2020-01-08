4c03f5b3dc46f98409c9c579fdadf70977ab59dc
John Murray <jmurrayufo@gmail.com>
2020-01-08 10:46:32 -0700

    Attempt to catch *huge* values from hitting mongo

--- poe_tracker/code/POE/trade/price.py
+++ poe_tracker/code/POE/trade/price.py
@@ -99,6 +99,10 @@ class Price:
 
         try:
             self.value = eval(match_obj.group(2)) if match_obj.group(2) is not None else 1
+            # Prevent > 64 bit numbers from being parsed if someone gives us a really stupid field
+            if self.value > 2**63:
+                self.log.error(f"Saw value of {self.value} and corrected it to {2**63}. Note was {note}")
+                self.value = 2**63
         except (SyntaxError, ZeroDivisionError):
             return False
         except TypeError:
