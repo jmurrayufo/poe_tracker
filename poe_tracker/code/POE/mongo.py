80cf2d4ba44d329002f6388eaffd0970638a80ea
John Murray <jmurrayufo@gmail.com>
2020-01-10 04:50:38 -0700

    Rework logging to be more clear on what's being done at boot

--- poe_tracker/code/POE/mongo.py
+++ poe_tracker/code/POE/mongo.py
@@ -50,13 +50,12 @@ class Mongo(metaclass=Singleton):
         try:
             # db.cache
             self.log.info("Setup db.cache")
-            self.log.info("Init cache indexes")
-            self.log.info("Create 'frameType_next_id_type' index")
+            self.log.info("db.cache: Create 'frameType_next_id_type' index")
             await self.db.cache.create_index(
                     [("name", 1)], 
                     name="name",
                     unique=True)
-            self.log.info("Init trade cache items")
+            self.log.info("db.cache: Init trade cache items")
             await self.db.cache.update_one(
                     {"name": "trade"},
                     {"$setOnInsert":
@@ -68,113 +67,74 @@ class Mongo(metaclass=Singleton):
                     upsert=True
             )
 
-
-            # db.index_markers
-            self.log.info("Setup db.index_markers")
-            self.log.info("Init index_markers indexes")
-            self.log.info("Create 'frameType_next_id_type' index")
-            await self.db.index_markers.create_index(
-                    [("frameType", 1),
-                     ("next_id", 1),
-                     ("type", 1),
-                    ], 
-                    name="frameType_next_id_type",
-                    unique=True)
-            self.log.info("Init index_markers items")
-            for i in range(10):
-                await self.db.index_markers.update_one(
-                        {"type": "implicitMods", "frameType": i},
-                        {
-                            "$setOnInsert": {
-                                "type": "implicitMods",
-                                "next_id": 0,
-                                "frameType":i,
-                            }
-                        },
-                        upsert=True,
-            )
-                await self.db.index_markers.update_one(
-                        {"type": "explicitMods", "frameType": i},
-                        {
-                            "$setOnInsert": {
-                                "type": "explicitMods",
-                                "next_id": 0,
-                                "frameType":i,
-                            }
-                        },
-                        upsert=True,
-            )
-
-
             # db.items
             self.log.info("Setup db.items")
-            self.log.info("Init items indexes")
-            self.log.info("Create 'createdAt' index")
+            self.log.info("db.items: Create 'createdAt' index")
             await self.db.items.create_index(
                     [('_createdAt', 1)],
                     name="createdAt"
             )
-            self.log.info("Create 'updatedAt' index")
+            self.log.info("db.items: Create 'updatedAt' index")
             await self.db.items.create_index(
                     [('_updatedAt', 1)],
                     name="updatedAt",
                     expireAfterSeconds=3*24*60*60
             )
-            self.log.info("Create 'category' index")
+            self.log.info("db.items: Create 'category' index")
             await self.db.items.create_index(
                     [('extended.category', 1)],
                     name="category",
             )
-            self.log.info("Create 'subcategories' index")
+            self.log.info("db.items: Create 'subcategories' index")
             await self.db.items.create_index(
                     [('extended.subcategories', 1)],
                     name="subcategories",
                     sparse=True,
             )
-            self.log.info("Create 'id' index")
+            self.log.info("db.items: Create 'id' index")
             await self.db.items.create_index(
                     [('id', 1)],
                     name="id",
                     unique=True,
             )
-            self.log.info("Create 'id_hashed' index")
+            self.log.info("db.items: Create 'id_hashed' index")
             await self.db.items.create_index(
                     [('id', 'hashed')],
                     name="id_hashed",
             )
-            self.log.info("Create 'league' index")
+            self.log.info("db.items: Create 'league' index")
             await self.db.items.create_index(
                     [('league', 1)],
                     name="league",
             )
-            self.log.info("Create 'name' index")
+            self.log.info("db.items: Create 'name' index")
             await self.db.items.create_index(
                     [('name', 1)],
                     name="name",
             )
-            self.log.info("Create 'typeLine' index")
+            self.log.info("db.items: Create 'typeLine' index")
             await self.db.items.create_index(
                     [('typeLine', 1)],
                     name="typeLine",
             )
-            self.log.info("Create 'note' index")
+            self.log.info("db.items: Create 'note' index")
             await self.db.items.create_index(
                     [('note', 1)],
                     name="note",
                     sparse=True,
             )
-            self.log.info("Create 'stash_id' index")
+            self.log.info("db.items: Create 'stash_id' index")
             await self.db.items.create_index(
                     [('stash_id', 1)],
                     name="stash_id",
             )
-            self.log.info("Create '_value_name' index")
+            self.log.info("db.items: Create '_value_name' index")
             await self.db.items.create_index(
                     [('_value_name', 1)],
                     name="_value_name",
                     sparse=True
             )
-            self.log.info("Create '_value' index")
+            self.log.info("db.items: Create '_value' index")
             await self.db.items.create_index(
                     [('_value', 1)],
                     name="_value",
@@ -218,18 +178,18 @@ class Mongo(metaclass=Singleton):
 
             # db.items.price.cache
             self.log.info("Setup db.items.price.cache")
-            self.log.info("Create 'typeLine' index")
+            self.log.info("db.items.price.cache: Create 'typeLine' index")
             await self.db.items.price.cache.create_index(
                     [('typeLine', 1)],
                     name="typeLine",
                     unique=True,
             )
-            self.log.info("Create '_value_name' index")
+            self.log.info("db.items.price.cache: Create '_value_name' index")
             await self.db.items.price.cache.create_index(
                     [('_value_name', 1)],
                     name="_value_name",
             )
-            self.log.info("Create '_value' index")
+            self.log.info("db.items.price.cache: Create '_value' index")
             await self.db.items.price.cache.create_index(
                     [('_value', 1)],
                     name="_value",
@@ -237,69 +197,68 @@ class Mongo(metaclass=Singleton):
 
             # db.items.mods
             self.log.info("Setup db.items.mods")
-            self.log.info("Init items indexes")
-            self.log.info("Create 'frameType' index")
+            self.log.info("db.items.mods: Create 'name' index")
             await self.db.items.mods.create_index(
-                    [('frameType', 1)],
-                    name="frameType",
-                    unique=True,
+                    [('name', 1)],
+                    name="name",
             )
-            self.log.info("Create 'id' index")
-            await self.db.items.mods.create_index(
-                    [('id', 1)],
-                    name="id",
-                    unique=True,
-            )
-            self.log.info("Create 'type' index")
+            self.log.info("db.items.mods: Create 'type' index")
             await self.db.items.mods.create_index(
                     [('type', 1)],
                     name="type",
-                    unique=True,
             )
-
+            self.log.info("db.items.mods: Create 'name, type, frameType' index")
+            await self.db.items.mods.create_index(
+                    [('name', 1),('type', 1)],
+                    name="name_type",
+                    unique=True
+            )
+            self.log.info("db.items.mods: Create 'name_hashed' index")
+            await self.db.items.mods.create_index(
+                    [('name', 'hashed')],
+                    name="name_hashed",
+            )
 
             # db.items.sold
             self.log.info("Setup db.items.sold")
-            self.log.info("Create 'id' index")
+            self.log.info("db.items.sold: Create 'id' index")
             await self.db.items.sold.create_index(
                     [('id', 1)],
                     name="id",
                     unique=True,
             )
-            self.log.info("Create 'updatedAt' index")
+            self.log.info("db.items.sold: Create 'updatedAt' index")
             await self.db.items.sold.create_index(
                     [('_updatedAt', 1)],
                     name="_updatedAt",
                     expireAfterSeconds=7*24*60*60
             )
 
-
             # db.stashes
             self.log.info("Setup db.stashes")
-            self.log.info("Init stashes indexes")
-            self.log.info("Create 'createdAt' index")
+            self.log.info("db.stashes: Create 'createdAt' index")
             await self.db.stashes.create_index(
                     [('_createdAt', 1)],
                     name="createdAt"
             )
-            self.log.info("Create 'updatedAt' index")
+            self.log.info("db.stashes: Create 'updatedAt' index")
             await self.db.stashes.create_index(
                     [('_updatedAt', 1)],
                     name="updatedAt",
                     expireAfterSeconds=3*24*60*60,
             )
-            self.log.info("Create 'id_hashed' index")
+            self.log.info("db.stashes: Create 'id_hashed' index")
             await self.db.stashes.create_index(
                     [('id', 'hashed')],
                     name="id_hashed",
             )
-            self.log.info("Create 'id' index")
+            self.log.info("db.stashes: Create 'id' index")
             await self.db.stashes.create_index(
                     [('id', 1)],
                     name="id",
                     unique=True,
             )
-            self.log.info("Create 'accountName' index")
+            self.log.info("db.stashes: Create 'accountName' index")
             await self.db.stashes.create_index(
                     [('accountName', 1)],
                     name="accountName",
@@ -307,7 +266,7 @@ class Mongo(metaclass=Singleton):
 
             # db.characters.xp
             self.log.info("Setup db.characters.xp")
-            self.log.info("Create 'date_name' index")
+            self.log.info("db.characters.xp: Create 'date_name' index")
             await self.db.characters.xp.create_index(
                     [('date', 1),('name',1)],
                     name="date_name",
