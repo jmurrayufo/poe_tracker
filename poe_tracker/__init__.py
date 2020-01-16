82487e852762c121ad76499be5d19d3c0bafce07
John Murray <jmurrayufo@gmail.com>
2020-01-15 20:02:03 -0700

    Update version, we are in alpha 0.2.0a now

--- poe_tracker/__init__.py
+++ poe_tracker/__init__.py
@@ -4,6 +4,6 @@ from collections import namedtuple
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 
-version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel='final', serial=0)
+version_info = VersionInfo(major=0, minor=2, micro=0, releaselevel='alpha', serial=0)
 
 __version__ = f'{version_info.major}.{version_info.minor}.{version_info.micro}'
--- run_bot_prod.sh
+++ run_bot_prod.sh
@@ -3,8 +3,8 @@
 # .creds file should look like this 
 # export CLIENT_TOKEN="LOTS OF NUMBERS THAT MAKE A TOKEN GO HERE"
 
-git checkout master
-git pull
+# git checkout master
+# git pull
 source ~/.ssh/poebot-prod.creds
 pipenv install
 pipenv run python3.7 -m poe_tracker --env prod "$@"
